a5549a189970c681759c86c90e0aed2ab9fd4e44
pas <pas@tutao.de>
2020-11-04 13:06:15 +0100

    WIP Nov4th template Feature

--- src/mail/TemplateDisplayer.js
+++ src/mail/TemplateDisplayer.js
@@ -1,46 +1,47 @@
 //@flow
 import m from "mithril"
 import {assertNotNull} from "../api/common/utils/Utils"
-import type {Template} from "../settings/TemplateViewList"
+import type {Template} from "../settings/TemplateListView"
 
 export type TemplateDisplayAttrs = {
-	template:Template
+	template: Template
 }
 
 export class TemplateDisplayer implements MComponent<TemplateDisplayAttrs> {
 	_id: string
 
 	view(vnode: Vnode<TemplateDisplayAttrs>): Children {
-		const {title, content, id} = vnode.attrs.template
+		const {title, content, id, index} = vnode.attrs.template
 		this._id = assertNotNull(id)
-		return m(".flex.flex-column", {style: {marginBottom: "3px", marginLeft: "8px", height: "90px"}}, [
-			m(".flex", [
-				id !== "" ? m(".b.teamLabel.pl-s.pr-s.border-radius.no-wrap.small.mr-s", {
-					style: {
-						fontSize: "12px",
-						width: "min-content",
-						height: "min-content",
-						marginLeft: "5px",
-						marginTop: "4px",
-						marginRight: "-4px",
-					}
-				}, "#" + this._id.toLowerCase()) : null,
+		return m(".flex.flex-column", {style: {marginBottom: "3px", marginLeft: "8px", height: "auto", width: "100%"}}, [
+			m(".flex.flex-column", [
 				m("", {
 					style: {
-						fontSize: "20px",
+						fontSize: "18px",
 						marginBottom: "-5px",
-						marginLeft: "8px"
 					}
-				}, title.length <= 40 ? title : title.substring(0, 40) + "...")
+				}, title.length <= 40 ? title : title.substring(0, 40) + "..."),
+				m(".flex    ",  {style:{marginTop: "4px"}} , [
+					id !== "" ? m(".b.teamLabel.pl-s.pr-s.border-radius.no-wrap.small.mr-s", {
+						style: {
+							fontSize: "12px",
+							width: "min-content",
+							height: "min-content",
+						}
+					}, "#" + this._id.toLowerCase()) : null,
+					m("", {style: {fontSize: "14px", fontWeight: "bold"}}, index === 0 ? "" : index !== 1 ? index + " languages" : index  + " language")
+				])
+
 			]),
-			m("", {
-				style: {
-					fontSize: "14px",
-					marginLeft: "7px",
-					height: "40px",
-					overflow: "hidden"
-				}
-			}, m.trust(content.substring(0, 220) + "...")),
-		])
+			// m("", {
+			// 	style: {
+			// 		fontSize: "14px",
+			// 		marginLeft: "7px",
+			// 		height: "40px",
+			// 		overflow: "hidden"
+			// 	}
+			// }, m.trust(content.substring(0, 220) + "...")),
+		],
+		)
 	}
 }

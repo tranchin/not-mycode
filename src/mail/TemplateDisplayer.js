44a03bf6b9690def1b44bf51c9f5a8a660a9dfcd
pas <pas@tutao.de>
2020-11-16 14:01:23 +0100

    Review ready Version Nov16

--- src/mail/TemplateDisplayer.js
+++ src/mail/TemplateDisplayer.js
@@ -19,34 +19,26 @@ export class TemplateDisplayer implements MComponent<TemplateDisplayAttrs> {
 		})
 		this._id = assertNotNull(groupKey)
 		return m(".flex.flex-column", {style: {marginBottom: "3px", marginLeft: "8px", height: "auto", width: "100%"}}, [
-			m(".flex.flex-column", [
-				m("", {
-					style: {
-						fontSize: "18px",
-						marginBottom: "-5px",
-					}
-				}, title.length <= 40 ? title : title.substring(0, 40) + "..."),
-				m(".flex    ",  {style:{marginTop: "4px"}} , [
-					groupKey !== "" ? m(".b.teamLabel.pl-s.pr-s.border-radius.no-wrap.small.mr-s", {
+				m(".flex.flex-column", [
+					m("", {
 						style: {
-							fontSize: "12px",
-							width: "min-content",
-							height: "min-content",
+							fontSize: "18px",
+							marginBottom: "-5px",
 						}
-					}, "#" + this._id.toLowerCase()) : null,
-					m("", {style: {fontSize: "14px", fontWeight: "bold"}}, this._length === 0 ? "" : this._length !== 1 ? this._length + " languages" : this._length  + " language")
+					}, title.length <= 40 ? title : title.substring(0, 40) + "..."),
+					m(".flex    ", {style: {marginTop: "4px"}}, [
+						groupKey !== "" ? m(".b.teamLabel.pl-s.pr-s.border-radius.no-wrap.small.mr-s", {
+							style: {
+								fontSize: "12px",
+								width: "min-content",
+								height: "min-content",
+							}
+						}, "#" + this._id.toLowerCase()) : null,
+						m("", {style: {fontSize: "14px", fontWeight: "bold"}}, this._length === 0 ? "" : this._length !== 1 ? this._length
+							+ " languages" : this._length + " language")
+					])
 				])
-
-			]),
-			// m("", {
-			// 	style: {
-			// 		fontSize: "14px",
-			// 		marginLeft: "7px",
-			// 		height: "40px",
-			// 		overflow: "hidden"
-			// 	}
-			// }, m.trust(content.substring(0, 220) + "...")),
-		],
+			],
 		)
 	}
 }

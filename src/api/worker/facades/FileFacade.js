a5bfbf44f0766d20103d698917324632c0c254b3
vis <vis@tutao.de>
2021-09-24 11:49:23 +0200

    Remove TODOs
    Converted to items on a TODO list on the bugtracker.

--- src/api/worker/facades/FileFacade.js
+++ src/api/worker/facades/FileFacade.js
@@ -198,8 +198,6 @@ export class FileFacade {
 				this._blobDownloaderNative(blobId, headers, body, server)
 		)
 
-		// make sure all suspensions have been handled
-
 		// Return first error code
 		const firstError = blobs.find(result => result.statusCode !== 200)
 		if (firstError) {
@@ -324,7 +322,7 @@ export class FileFacade {
 		})
 		const literalGetData = await encryptAndMapToLiteral(BlobDataGetTypeModel, getData, null)
 		const body = JSON.stringify(literalGetData)
-		const server = servers[0] // TODO: Use another server if download fails
+		const server = servers[0]
 
 		return blobDownloader(blobId, headers, body, server)
 	}
@@ -371,7 +369,7 @@ export class FileFacade {
 	}
 
 	// ↑↑↑ Download ↑↑↑
-	//////////////////////////////////////////////////
+	//-------------------------------------------------
 	// ↓↓↓ Upload ↓↓↓
 
 	async uploadFile(file: DataFile | FileReference, sessionKey: Aes128Key): Promise<Id> {
@@ -456,7 +454,6 @@ export class FileFacade {
 		data: T, size: number, sessionKey: Aes128Key, encrypter: FileEncryptor<T>, splitter: BlobSplitter<T>, uploader: BlobUploader<T>): Promise<Id> {
 		const encrypted = await encrypter(sessionKey, data)
 
-		// TODO: Watch for timeout of the access token when uploading many chunks
 		const {storageAccessToken, servers} = accessInfo
 		const headers = Object.assign({
 			storageAccessToken,

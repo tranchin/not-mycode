07d48775c308fbf20cd04cdbf852eefc9fa9b8e5
vis <vis@tutao.de>
2021-10-12 12:19:09 +0200

    Remove FIXME
    It already works. For free!

--- src/mail/export/Bundler.js
+++ src/mail/export/Bundler.js
@@ -54,7 +54,6 @@ export function makeMailBundle(mail: Mail, entityClient: EntityClient, mailFacad
 		                                    }).text
 	                                    )
 
-	// FIXME blob attachments
 	const attachmentsPromise: Promise<Array<DataFile>> =
 		promiseMap(mail.attachments, fileId => entityClient.load(FileTypeRef, fileId)
 		                                                   .then((file) => fileFacade.downloadFileContent(file)))

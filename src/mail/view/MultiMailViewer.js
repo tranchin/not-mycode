bastiencaillot/tutanota

Fix tests and bundles

src/mail/view/MultiMailViewer.js
@@ -118,7 +118,9 @@ export class MultiMailViewer {
 				{
 					label: "export_action",
 					click: this._actionBarAction(
-						(mails) => showProgressDialog("pleaseWait_msg", exportMails(mails, locator.entityClient, worker.fileFacade))
+						(mails) => showProgressDialog("pleaseWait_msg",
+							exportMails(mails, locator.entityClient, worker.mailFacade, worker.fileFacade)
+						)
 					),
 					icon: () => Icons.Export,
 					type: ButtonType.Dropdown,

tranchin/not-mycode

src/mail/view/MultiMailViewer.js
@@ -115,7 +115,9 @@ export class MultiMailViewer {
 				{
 					label: "export_action",
 					click: this._actionBarAction(
-						(mails) => showProgressDialog("pleaseWait_msg", exportMails(mails, locator.entityClient, locator.mailFacade, locator.fileFacade))
+						(mails) => showProgressDialog("pleaseWait_msg",
+							exportMails(mails, locator.entityClient, locator.mailFacade, locator.fileFacade)
+						)
 					),
 					icon: () => Icons.Export,
 					type: ButtonType.Dropdown,
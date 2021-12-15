fairhopeweb/tutanota

[desktop] Try using existing blob files before downloading

src/api/worker/facades/FileFacade.js
@@ -32,7 +32,7 @@ import type {BlobAccessInfo} from "../../entities/sys/BlobAccessInfo"
 import {_TypeModel as BlobDataGetTypeModel, createBlobDataGet} from "../../entities/storage/BlobDataGet"
 import {createBlobWriteData} from "../../entities/storage/BlobWriteData"
 import {createTypeInfo} from "../../entities/sys/TypeInfo"
-import {uint8ArrayToBase64, uint8ArrayToHex} from "../../common/utils/Encoding"
+import {base64ToUint8Array, uint8ArrayToBase64, uint8ArrayToHex} from "../../common/utils/Encoding"
 import {TypeRef} from "../../common/utils/TypeRef"
 import type {TypeModel} from "../../common/EntityTypes"
 import {LoginFacadeImpl} from "./LoginFacade"
@@ -66,7 +66,11 @@ type BlobUploader<T: Uint8Array | FileReference> = (url: string, headers: Params
 
 
 function _getBlobIdFromData(blob: Uint8Array): string {
-	return uint8ArrayToBase64(hash(blob).slice(0, 6))
+	return _getBlobIdFromHash(hash(blob))
+}
+
+function _getBlobIdFromHash(blobHash: Uint8Array): string {
+	return uint8ArrayToBase64(blobHash.slice(0, 6))
 }
 
 export class FileFacade {
@@ -316,10 +320,28 @@ export class FileFacade {
 	}
 
 	async _blobDownloaderNative(blobId: BlobId, headers: Params, body: string, server: TargetServer): Promise<NativeDownloadResult> {
-		const filename = uint8ArrayToHex(blobId.blobId) + ".blob"
+		const blobFilename = uint8ArrayToHex(blobId.blobId) + ".blob"
+		const fileUri = await fileApp.getTempFileUri(blobFilename)
+
+		// Check if we have the blob in the filesystem
+		try {
+			const existingBlobHash = await fileApp.hashFile(fileUri)
+			const existingBlobId = base64ToUint8Array(existingBlobHash).slice(0, 6)
+			if (arrayEquals(blobId.blobId, existingBlobId)) {
+				console.log(`using existing blob file: ${blobFilename}`)
+				return {
+					statusCode: 200,
+					encryptedFileUri: fileUri
+				}
+			}
+
+		} catch(e) {
+			// could not find existing blob
+		}
+
 		const serviceUrl = new URL(getRestPath(StorageService.BlobService), server.url)
 		const url = addParamsToUrl(serviceUrl, {"_body": body})
-		return fileApp.download(url.toString(), headers, filename)
+		return fileApp.download(url.toString(), headers, fileUri)
 	}
 
 	async _getDownloadToken(readArchiveId: Id): Promise<BlobAccessInfo> {

src/desktop/DesktopDownloadManager.js
@@ -13,6 +13,9 @@ import {promises as fs} from "fs"
 import type {DateProvider} from "../calendar/date/CalendarUtils"
 import {CancelledError} from "../api/common/error/CancelledError"
 import type {NativeDownloadResult} from "../native/common/FileApp"
+import {hash} from "../api/worker/crypto/Sha256"
+import {uint8ArrayToBase64} from "../api/common/utils/Encoding"
+
 
 const TAG = "[DownloadManager]"
 
@@ -56,14 +59,28 @@ export class DesktopDownloadManager {
 		       .on("spellcheck-dictionary-download-failure", (ev, lcode) => log.debug(TAG, "spellcheck-dictionary-download-failure", lcode))
 	}
 
-	async downloadNative(url: string, headers: Params, filename: string): Promise<NativeDownloadResult> {
+
+	/**
+	 * SHA256 of the file found at given URI
+	 * @throws Error if file is not found
+	 */
+	async hashFile(fileUri: string): Promise<string> {
+		const data = await this._fs.promises.readFile(fileUri)
+		const checksum = hash(data)
+		return uint8ArrayToBase64(checksum)
+	}
+
+	async getTempFileUri(filename: string): Promise<string> {
+		const downloadDirectory = await this.getTutanotaTempDirectory("download")
+		return path.join(downloadDirectory, filename)
+	}
+
+	async download(url: string, headers: Params, filename: string): Promise<NativeDownloadResult> {
 		return new Promise(async (resolve, reject) => {
-			const downloadDirectory = await this.getTutanotaTempDirectory("download")
-			const encryptedFileUri = path.join(downloadDirectory, filename)
-			const fileStream = this._fs.createWriteStream(encryptedFileUri)
+			const fileStream = this._fs.createWriteStream(filename)
 			                       .on('close', () => resolve({
 				                       statusCode: 200,
-				                       encryptedFileUri: encryptedFileUri
+				                       encryptedFileUri: filename
 			                       }))
 
 			let cleanup = e => {
@@ -72,7 +89,7 @@ export class DesktopDownloadManager {
 				          .on('close', () => { // file descriptor was released
 					          fileStream.removeAllListeners('close')
 					          // remove file if it was already created
-					          this._fs.promises.unlink(encryptedFileUri)
+					          this._fs.promises.unlink(filename)
 					              .catch(noOp)
 					              .then(() => reject(e))
 				          })

tranchin/not-mycode

src/api/worker/facades/FileFacade.js
@@ -9,7 +9,7 @@ import {_TypeModel as FileTypeModel} from "../../entities/tutanota/File"
 import {_TypeModel as FileDataTypeModel, FileDataTypeRef} from "../../entities/tutanota/FileData"
 import {
 	arrayEquals,
-	assertNotNull,
+	assertNotNull, base64ToUint8Array,
 	concat,
 	filterInt,
 	isEmpty,
@@ -69,7 +69,11 @@ type BlobUploader<T: Uint8Array | FileReference> = (url: string, headers: Params
 
 
 function _getBlobIdFromData(blob: Uint8Array): string {
-	return uint8ArrayToBase64(sha256Hash(blob).slice(0, 6))
+	return _getBlobIdFromHash(sha256Hash(blob))
+}
+
+function _getBlobIdFromHash(blobHash: Uint8Array): string {
+	return uint8ArrayToBase64(blobHash.slice(0, 6))
 }
 
 export class FileFacade {
@@ -330,10 +334,31 @@ export class FileFacade {
 	}
 
 	async _blobDownloaderNative(blobId: BlobId, headers: Params, body: string, server: TargetServer): Promise<DownloadTaskResponse> {
-		const filename = uint8ArrayToHex(blobId.blobId) + ".blob"
+		const blobFilename = uint8ArrayToHex(blobId.blobId) + ".blob"
+		const fileUri = await this._fileApp.getTempFileUri(blobFilename)
+
+		// Check if we have the blob in the filesystem
+		try {
+			const existingBlobHash = await this._fileApp.hashFile(fileUri)
+			const existingBlobId = base64ToUint8Array(existingBlobHash).slice(0, 6)
+			if (arrayEquals(blobId.blobId, existingBlobId)) {
+				console.log(`using existing blob file: ${blobFilename}`)
+				return {
+					statusCode: 200,
+					encryptedFileUri: fileUri,
+					errorId: null,
+					precondition: null,
+					suspensionTime: null,
+					responseBody: null
+				}
+			}
+
+		} catch(e) {
+			// could not find existing blob
+		}
 		const serviceUrl = new URL(getRestPath(StorageService.BlobService), server.url)
 		const url = addParamsToUrl(serviceUrl, {"_body": body})
-		return this._fileApp.download(url.toString(), headers, filename)
+		return this._fileApp.download(url.toString(), headers, fileUri)
 	}
 
 	async _getDownloadToken(readArchiveId: Id): Promise<BlobAccessInfo> {

src/desktop/DesktopDownloadManager.js
@@ -2,7 +2,7 @@
 import type {ElectronSession} from 'electron'
 import type {DesktopConfig} from "./config/DesktopConfig"
 import path from "path"
-import {assertNotNull, downcast, noOp} from "@tutao/tutanota-utils"
+import {assertNotNull, downcast, noOp, uint8ArrayToBase64} from "@tutao/tutanota-utils"
 import {lang} from "../misc/LanguageViewModel"
 import type {DesktopNetworkClient} from "./DesktopNetworkClient"
 import {FileOpenError} from "../api/common/error/FileOpenError"
@@ -12,8 +12,8 @@ import type {DesktopUtils} from "./DesktopUtils"
 import {promises as fs} from "fs"
 import type {DateProvider} from "../calendar/date/CalendarUtils"
 import {CancelledError} from "../api/common/error/CancelledError"
-import {addParamsToUrl} from "../api/worker/rest/RestClient"
 import type {DownloadTaskResponse} from "../native/common/FileApp"
+import {sha256Hash} from "@tutao/tutanota-crypto"
 
 const TAG = "[DownloadManager]"
 
@@ -55,15 +55,29 @@ export class DesktopDownloadManager {
 		       .on("spellcheck-dictionary-download-failure", (ev, lcode) => log.debug(TAG, "spellcheck-dictionary-download-failure", lcode))
 	}
 
-	async downloadNative(url: string, headers: Params, filename: string): Promise<DownloadTaskResponse> {
+
+	/**
+	 * SHA256 of the file found at given URI
+	 * @throws Error if file is not found
+	 */
+	async hashFile(fileUri: string): Promise<string> {
+		const data = await this._fs.promises.readFile(fileUri)
+		const checksum = sha256Hash(data)
+		return uint8ArrayToBase64(checksum)
+	}
+
+	async getTempFileUri(filename: string): Promise<string> {
+		const downloadDirectory = await this.getTutanotaTempDirectory("download")
+		return path.join(downloadDirectory, filename)
+	}
+
+	async download(url: string, headers: Params, filename: string): Promise<DownloadTaskResponse> {
 		return new Promise(async (resolve, reject) => {
-			const downloadDirectory = await this.getTutanotaTempDirectory("download")
-			const encryptedFileUri = path.join(downloadDirectory, filename)
-			const fileStream = this._fs.createWriteStream(encryptedFileUri)
+			const fileStream = this._fs.createWriteStream(filename)
 			                       .on('close', () => resolve({
-					                       statusCode: 200,
-					                       encryptedFileUri: encryptedFileUri,
-										errorId: null,
+				                       statusCode: 200,
+				                       encryptedFileUri: filename,
+				                       errorId: null,
 				                       precondition: null,
 				                       suspensionTime: null,
 				                       responseBody: null,
@@ -75,7 +89,7 @@ export class DesktopDownloadManager {
 				          .on('close', () => { // file descriptor was released
 					          fileStream.removeAllListeners('close')
 					          // remove file if it was already created
-					          this._fs.promises.unlink(encryptedFileUri)
+					          this._fs.promises.unlink(filename)
 					              .catch(noOp)
 					              .then(() => reject(e))
 				          })
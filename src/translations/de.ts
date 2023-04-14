d406b0974115967b82c16309c218a1ba4d0de2bc
vis <vis@tutao.de>
2023-04-14 10:48:11 +0200

    Add "load all" translation key

--- src/translations/de.ts
+++ src/translations/de.ts
@@ -1643,6 +1643,7 @@ export default {
 		"yourCalendars_label": "Deine Kalender",
 		"yourFolders_action": "DEINE ORDNER",
 		"yourMessage_label": "Deine Nachricht",
-		"you_label": "Du"
+		"you_label": "Du",
+		"loadAll_action": "Alles laden"
 	}
 }

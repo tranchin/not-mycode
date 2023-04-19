e9f0b1f4b00d4756432094e5f8959a507ec879de
map <mpfau@users.noreply.github.com>
2023-04-19 09:35:28 +0200

    fixed tests

--- test/tests/subscription/PriceUtilsTest.ts
+++ test/tests/subscription/PriceUtilsTest.ts
@@ -1,11 +1,12 @@
 import o from "ospec"
 import { asPaymentInterval, formatMonthlyPrice, formatPrice, PaymentInterval, PriceAndConfigProvider } from "../../../src/subscription/PriceUtils.js"
 import { clone } from "@tutao/tutanota-utils"
-import { LegacySubscriptionType, UpgradePriceType } from "../../../src/subscription/FeatureListProvider"
+import { UpgradePriceType } from "../../../src/subscription/FeatureListProvider"
 import { lang } from "../../../src/misc/LanguageViewModel"
 import en from "../../../src/translations/en"
 import { ProgrammingError } from "../../../src/api/common/error/ProgrammingError.js"
 import { createPriceMock, PLAN_PRICES } from "./priceTestUtils.js"
+import { SubscriptionType } from "../../../src/api/common/TutanotaConstants.js"
 
 o.spec("price utils getSubscriptionPrice", function () {
 	let provider: PriceAndConfigProvider
@@ -18,53 +19,53 @@ o.spec("price utils getSubscriptionPrice", function () {
 	o("getSubscriptionPrice premium yearly price", function () {
 		// the return value is not rounded, but formatPrice handles that
 		o(
-			formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanReferencePrice), false),
+			formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanReferencePrice), false),
 		).equals("14.40")
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(12)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(12)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(240)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(12)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(12)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(12)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(240)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(12)
 	})
 	o("getSubscriptionPrice premium monthly price", function () {
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanReferencePrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(24)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanReferencePrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(24)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(1.2)
 	})
 	o("getSubscriptionPrice Premium discount yearly", async function () {
 		const discountPlanPrices = clone(PLAN_PRICES)
 		discountPlanPrices.Premium.firstYearDiscount = "12"
 		const provider = await createPriceMock(discountPlanPrices)
 		o(
-			formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanReferencePrice), false),
+			formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanReferencePrice), false),
 		).equals("14.40")
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(0)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(12)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(240)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(12)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(0)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(12)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(240)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(12)
 	})
 	o("getSubscriptionPrice Pro discount yearly", async function () {
 		const discountPlanPrices = clone(PLAN_PRICES)
 		discountPlanPrices.Pro.firstYearDiscount = "84"
 		const provider = await createPriceMock(discountPlanPrices)
-		o(formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Pro, UpgradePriceType.PlanReferencePrice), false)).equals(
+		o(formatPrice(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Pro, UpgradePriceType.PlanReferencePrice), false)).equals(
 			"100.80",
 		)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Pro, UpgradePriceType.PlanActualPrice)).equals(0)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Pro, UpgradePriceType.AdditionalUserPrice)).equals(48)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Pro, UpgradePriceType.ContactFormPrice)).equals(240)
-		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, LegacySubscriptionType.Pro, UpgradePriceType.PlanNextYearsPrice)).equals(84)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Pro, UpgradePriceType.PlanActualPrice)).equals(0)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Pro, UpgradePriceType.AdditionalUserPrice)).equals(48)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Pro, UpgradePriceType.ContactFormPrice)).equals(240)
+		o(provider.getSubscriptionPrice(PaymentInterval.Yearly, SubscriptionType.Pro, UpgradePriceType.PlanNextYearsPrice)).equals(84)
 	})
 	o("getSubscriptionPrice Premium discount monthly", async function () {
 		const discountPlanPrices = clone(PLAN_PRICES)
 		discountPlanPrices.Premium.firstYearDiscount = "12"
 		const provider = await createPriceMock(discountPlanPrices)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanReferencePrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(1.2)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(24)
-		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, LegacySubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanReferencePrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanActualPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.AdditionalUserPrice)).equals(1.2)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.ContactFormPrice)).equals(24)
+		o(provider.getSubscriptionPrice(PaymentInterval.Monthly, SubscriptionType.Premium, UpgradePriceType.PlanNextYearsPrice)).equals(1.2)
 	})
 	o("formatMonthlyPrices", function () {
 		o(formatMonthlyPrice(0, 12)).equals("€0")
